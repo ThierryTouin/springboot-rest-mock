31414ad0f995c040880c011ecd8e20817ca66213
ttouin <ttouin>
2015-12-08 17:27:30 +0100

    add model project

--- springboot-rest-secure-mock-ws/src/main/java/fr/touin/thierry/service/rest/RestWebService.java
+++ springboot-rest-secure-mock-ws/src/main/java/fr/touin/thierry/service/rest/RestWebService.java
@@ -2,20 +2,15 @@ package fr.touin.thierry.service.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.touin.thierry.service.rest.bean.UserBiz;
+import fr.touin.thierry.mock.bean.UserBiz;
 import fr.touin.thierry.service.rest.exception.BusinessException;
 import fr.touin.thierry.service.rest.exception.TechnicalException;
-
-import java.security.Principal;
-import java.util.Date;
 
 @RequestMapping("/rest")
 @RestController()
