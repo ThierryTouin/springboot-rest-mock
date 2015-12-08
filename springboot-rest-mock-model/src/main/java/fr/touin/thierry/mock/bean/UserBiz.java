127a15d9dd0bb6686432e4146f96bde5777630bc
ttouin <ttouin>
2015-12-08 17:28:54 +0100

    add commit project

---
+++ springboot-rest-mock-model/src/main/java/fr/touin/thierry/mock/bean/UserBiz.java
@@ -0,0 +1,48 @@
+package fr.touin.thierry.mock.bean;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "userxml")
+//@XmlRootElement
+public class UserBiz {
+
+    private Integer id;
+
+    private String  name;
+
+    public UserBiz() {
+    }
+
+//    public UserBiz(String userBiz) {
+//       //this = (UserBiz) Jacksson.jsonToObject(userBiz);
+//    	System.out.println("userBizEnJSon=" + userBiz);
+//    	this.name = userBiz;
+//    }    
+   
+    public UserBiz(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{id=%s,name=%s}", id, name);
+    }
+
+}
