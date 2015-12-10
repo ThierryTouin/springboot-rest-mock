e1a11212a7fb4d5b08ffd225bae72266b7260931
ttouin <ttouin>
2015-12-10 14:16:24 +0100

    delete secure

---
+++ springboot-rest-mock-ws/src/main/java/fr/touin/thierry/service/rest/exception/TechnicalException.java
@@ -0,0 +1,49 @@
+package fr.touin.thierry.service.rest.exception;
+
+/**
+ * Classe qui représente une exception technique.
+ * 
+ *
+ */
+public class TechnicalException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1322014819922214523L;
+
+	/**
+	 * Constructeur.
+	 * 
+	 * @param cause
+	 *            Exception
+	 */
+	public TechnicalException(Exception cause) {
+		super(cause);
+	}
+
+	/**
+	 * Constructeur.
+	 * 
+	 * @param message
+	 *            message decrivant l'erreur
+	 */
+	public TechnicalException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructeur.
+	 * 
+	 * @param message
+	 *            message qui traduit l erreur.
+	 *
+	 * @param cause
+	 *            cause de l'erreur (la stack trace).
+	 */
+
+	public TechnicalException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
